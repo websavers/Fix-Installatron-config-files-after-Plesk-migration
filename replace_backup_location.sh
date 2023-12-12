364f084d602c0a31c570babc8a16a1b34fad1d68
Jordan Schelew <jordan@websavers.ca>
2023-12-12 17:01:08 -0400

    Add script to replace a backup location with a new one

---
+++ replace_backup_location.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This can be run after you've moved backup files to a new server location.
+# If you had to create a new location in Installatron (ex: location type), running
+# this will tell Installatron where to find the backup files
+# After running, you won't visually see the change in the backups list
+# but downloading backups will work.
+
+# IMPORTANT: You must manually find and set the location IDs below
+# The easiest way to find these is to go to:
+# Settings > Backup > Locations > click on the 'X apps' link
+# The heading will be - Search: bkloc:8c2b7bd2d90245e58c529a0a267d5e5f
+# The value after bkloc: is your location ID.
+
+OLD_BACKUP_LOCATION=dc1dac9a010d491bae7721f339f28272
+NEW_BACKUP_LOCATION=8c2b7bd2d90245e58c529a0a267d5e5f
+
+BACKUP_CFG_FILES=$(grep -l -m 1 $OLD_BACKUP_LOCATION /var/www/vhosts/*/.appdata/backups/*)
+APP_CFG_FILES=$(grep -l -m 1 $OLD_BACKUP_LOCATION /var/www/vhosts/*/.appdata/current/*)
+CFG_FILES="$BACKUP_CFG_FILES"$'\n'"$APP_CFG_FILES"
+
+echo "Replacing backup locations for all backups and apps"
+for cfg_file in $CFG_FILES
+do
+    sed -i "s/$OLD_BACKUP_LOCATION/$NEW_BACKUP_LOCATION/g" $cfg_file
+done
+
+echo "Running Installatron Cache Update"
+DATE=$(date -I)
+mv -f /var/installatron/data.db /var/installatron/data.db.bak$DATE
+rm -f /var/installatron/data.db-*
+/usr/local/installatron/installatron --repair --recache
+/usr/local/installatron/installatron --send-update-report
